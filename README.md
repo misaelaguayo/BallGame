8fa3304d3eb44fc2cf696b3cad1a888613c75063
Misael Aguayo <aguayo.misael@gmail.com>
2023-04-09 11:42:53 -0500

    Compile with cmake

---
+++ README.md
@@ -0,0 +1,10 @@
+# Requirements
+
+- SDL2
+    - SDL2 as well as libraries
+- cmake
+
+# building
+
+- `cmake .`
+- `make`
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Compile game.cpp
-g++ -c game.cpp
-
-# Link game.o and create executable
-g++ game.o -o game -lSDL2main -lSDL2
